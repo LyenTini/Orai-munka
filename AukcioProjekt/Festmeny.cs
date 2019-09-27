a6d539434644f07c9d8004f5df0af79887d42cd1
Lyen <julien.apatini@gmail.com>
2019-09-27 12:30:13 +0200

    Nem kész (Hibás)

--- AukcioProjekt/Festmeny.cs
+++ AukcioProjekt/Festmeny.cs
@@ -1,45 +1,99 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace AukcioProjekt
 {
-    class Festmeny
+    public class Festmeny
     {
         private string cim;
         private string festo;
         private string stilus;
         private int licitekSzama = 0;
-        private int legmagasabbLicit=0;
-        private DateTime legutolsoLicitIdeje;
-        private bool elkelt=false;
+        private int legmagasabbLicit = 0;
+        private DateTime legutolsoLicitIdeje=DateTime.Now;
+        private bool elkelt = false;
 
-        public Festmeny(string cim, string festo, string stilus, int licitekSzama, int legmagasabbLicit, DateTime legutolsoLicitIdeje, bool elkelt)
+        public Festmeny(string cim, string festo, string stilus)
         {
-            this.Cim = cim;
-            this.Festo = festo;
-            this.Stilus = stilus;
-            this.LicitekSzama = licitekSzama;
-            this.LegmagasabbLicit = legmagasabbLicit;
-            this.LegutolsoLicitIdeje = legutolsoLicitIdeje;
-            this.Elkelt = elkelt;
+            this.cim = cim;
+            this.festo = festo;
+            this.stilus = stilus;
+
         }
 
-        public string Cim { get => cim; set => cim = value; }
-        public string Festo { get => festo; set => festo = value; }
-        public string Stilus { get => stilus; set => stilus = value; }
         public int LicitekSzama { get => licitekSzama; set => licitekSzama = value; }
         public int LegmagasabbLicit { get => legmagasabbLicit; set => legmagasabbLicit = value; }
         public DateTime LegutolsoLicitIdeje { get => legutolsoLicitIdeje; set => legutolsoLicitIdeje = value; }
         public bool Elkelt { get => elkelt; set => elkelt = value; }
 
+
+
+        public void Beolvas()
+        {
+            List<Festmeny> fajl = new List<Festmeny>();
+            try
+            {
+                
+                StreamReader olvas = new StreamReader("Festmeny.csv",Encoding.UTF8);
+                while (!olvas.EndOfStream)
+                {
+                    
+                    string sor = olvas.ReadLine();
+                    string[] st = sor.Split(';');
+                    string festo = st[0];
+                    string cim = st[1];
+                    string stilus = st[2];
+
+                    
+                }
+                olvas.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Hiba "+e);
+            }
+            foreach (string s in fajl)
+            {
+                fajl.Add(s);
+            }
+        }
+
         public void Kiir()
         {
+            if (elkelt==true)
+            {
+                Console.WriteLine("Elkelt.");
+            }
+            else if(elkelt==false)
+            {
+                Console.WriteLine("Nem kelt el.");
+            }
+            Console.WriteLine("{0}: {1} ({2})",this.festo,this.cim,this.stilus);
+            Console.WriteLine("{0}$ - {1} (összesen: {2} db)",this.legmagasabbLicit,legutolsoLicitIdeje,licitekSzama);
             
         }
 
+        //public override string ToString()
+        //{
+        //    return base.ToString();
+        //}
+
+        //public static List<Festmeny> Beolvas()
+        //{
+        //    string[] festmeny = File.ReadAllLines("festmeny.csv");
+        //    List<Festmeny> lista = List<Festmeny>();
+        //    foreach (var f in festmeny)
+        //    {
+        //        string[] tomb = f.Split(';');
+        //        Festmeny f1 = new Festmeny(tomb[0], tomb[1], tomb[2]);
+        //        lista.Add(f1);
+        //    }
+        //    return lista;
+        //}
 
     }
 }
--- AukcioProjekt/Program.cs
+++ AukcioProjekt/Program.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AukcioProjekt;
 
 namespace AukcioProjekt
 {
     class Program
     {
+        static List<Festmeny> lista2 = new List<Festmeny>();
+        public void Hozzaad()
+        {
+            
+            foreach (var f in lista)
+            {
+                Festmeny f1 = new Festmeny("Fék", "Ákos", "Expresszionizmus");
+                Festmeny f2 = new Festmeny("Menny", "Miklós", "Barokk");
+                lista.Add(f1);
+                lista.Add(f2);
+            }
+            Console.WriteLine(lista);
+
+        }
+        static List<Festmeny> lista = new List<Festmeny>();
         static void Main(string[] args)
         {
-            Console.WriteLine("Próba");
+            Festmeny f1 = new Festmeny("Kém", "András", "Futurizmus");
+            Festmeny f2 = new Festmeny("Ég", "Ágnes", "Futurizmus");
+            f1.Kiir();
+            Console.WriteLine();
+            f2.Kiir();
+            
+
+
+
 
             Console.ReadLine();
         }
